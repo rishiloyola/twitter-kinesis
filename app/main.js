2f1cd374626be7027a2fb66e749bfcc0110b6921
Rishi <rishiloyola98245@gmail.com>
2016-06-04 15:19:05 +0530

    Integrate foursquare api

--- app/main.js
+++ app/main.js
@@ -3,20 +3,12 @@
 
 var Twit = require('twit');
 var request = require("request");
-var Appbase = require("appbase-js");
 var fs = require("fs");
-var content = fs.readFileSync("config.json");
-var jsonContent = JSON.parse(content);
 
-var AWS = require('aws-sdk');
-var config = require('../bin/config.js');
 var producer = require('../bin/producer.js');
 
-const HOSTNAME = jsonContent.appbase.hostname
-const APPNAME = jsonContent.appbase.appname
-const USERNAME = jsonContent.appbase.username
-const PASSWORD = jsonContent.appbase.password
-
+var content = fs.readFileSync("config.json");
+var jsonContent = JSON.parse(content);
 
 //Required authetication of twitter api
 var T = new Twit({
@@ -29,43 +21,51 @@ var T = new Twit({
 
 //Filter tweets related to swarmapp
 var stream = T.stream('statuses/filter', { track: 'swarmapp', language: 'en' });
-
-
-var appbaseObj = new Appbase({
-    "url": "https://scalr.api.appbase.io",
-    "appname": APPNAME,
-    "username": USERNAME,
-    "password": PASSWORD
-});
-
-var kinesis = new AWS.Kinesis({region : config.kinesis.region});
-var streamStatus = producer(kinesis, config.sampleProducer).run();
+var streamStatus = producer().run();
 
 //Streaming of twitter tweets
 stream.on('tweet', function(tweet) {
 
-  if (verifyTweets(tweet)) {
+  if (verifyTweets(tweet)){
 
-    var data = {
-      text: tweet.text,
-      name: tweet.user.name,
-      location: tweet.user.location
-    }
+    var swarmappUrl = tweet.entities.urls[0].display_url;
+    //extracting id from that url
+    var FSid = swarmappUrl.split("/")[2];
+    //created url to get data of that checkin from swarmapp
+    var FSurl = "https://api.foursquare.com/v2/checkins/resolve?shortId="+FSid+"&oauth_token="+jsonContent.foursquare.oauth_token+"&v=20150919";
+    //Getting data from foursquare
+    
+    request(FSurl, function(error, response, body) {
+        if(verifyFoursquare(body,error)){
+            var parsedbody = JSON.parse(body);
 
-    if (streamStatus){
-      producer(kinesis, config.sampleProducer).sendData(data);
-    }
+            if(parsedbody.meta.code==200){
+                var data = {
+                  shout: parsedbody.response.checkin.shout,
+                  city: parsedbody.response.checkin.venue.location.city,
+                  category: parsedbody.response.checkin.venue.categories[0].shortName,
+                  venue: parsedbody.response.checkin.venue.name,
+                  url: swarmappUrl,
+                  username: parsedbody.response.checkin.user.firstName,
+                  state: parsedbody.response.checkin.venue.location.state,
+                  country: parsedbody.response.checkin.venue.location.country
+                };
+                if (streamStatus){
+                  producer().sendData(data);
+                }
+            }
+        }
+    });
   }
-
 });
 
 
 function verifyTweets(tweet) {
 
     if (tweet) {
-        if (tweet.text) {
-            if (tweet.user && tweet.user.name) {
-                if (tweet.user.location) {
+        if (tweet.entities) {
+            if (tweet.entities.urls[0]) {
+                if (tweet.entities.urls[0].display_url) {
                     return true;
                 }
             }
@@ -73,4 +73,28 @@ function verifyTweets(tweet) {
     }
 
     return false;
+}
+
+function verifyFoursquare(fsdata,error){
+  if(fsdata && !error){
+      var parsedbody = JSON.parse(fsdata);
+      if(parsedbody.response){
+        if(parsedbody.response.checkin){
+          if(parsedbody.response.checkin.venue && parsedbody.response.checkin.user){
+            if(parsedbody.response.checkin.venue.location.city && parsedbody.response.checkin.user.firstName && parsedbody.response.checkin.venue.location.state && parsedbody.response.checkin.venue.location.country){
+              if(parsedbody.response.checkin.venue.categories[0]){
+                if(parsedbody.response.checkin.venue.categories[0].shortName){
+                  if(parsedbody.response.checkin.venue.name){
+                    if(parsedbody.response.checkin.shout){
+                      return true;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  return false;  
 }
